b24888c1cc03f49da3b8d7ddf2cd7bbec2a5b651
Oleg Sorokin <oleg.sorokin@rtbid.ru>
2020-05-19 14:38:57 +0600

    Add the TaskController

---
+++ src/controllers/task.js
@@ -0,0 +1,50 @@
+import TaskComponent from "../components/task.js";
+import TaskEditComponent from "../components/task-edit.js";
+import {render, replace, RenderPosition} from "../utils/render.js";
+
+
+export default class TaskController {
+  constructor(container) {
+    this._container = container;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  render(task) {
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+
+  _replaceTaskToEdit() {
+    replace(this._taskEditComponent, this._taskComponent);
+  }
+
+  _replaceEditToTask() {
+    replace(this._taskComponent, this._taskEditComponent);
+  }
+}
